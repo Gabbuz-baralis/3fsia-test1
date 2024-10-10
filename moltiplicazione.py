19ab1f7187d74e599f307bd54a030ab3bc131446
Gabbuz-baralis <gabriele.baralis@iismajoranamoncalieri.edu.it>
2024-10-10 11:57:40 +0000

    modifica python

---
+++ moltiplicazione.py
@@ -0,0 +1,12 @@
+"""
+programma che legge due numeri in imput 
+e li moltiplica se sono entrambi positivi
+"""
+
+x = input("inserisci il primo numero(positivo): ")
+
+y = input("inserisci il secondo numero(positivo): ")
+
+x = int(x)
+
+y = int(y)
